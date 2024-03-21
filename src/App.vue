f2c94ac7b0a6889edbd09842a967b4d1c7910a13
Paula <paula.gdopico@gmail.com>
2024-03-21 18:51:11 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
   <ion-app>
+    <ion-menu content-id="main-content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Menu Content</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">This is the menu content.</ion-content>
+  </ion-menu>
+  <ion-page id="main-content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+        <ion-title>Menu</ion-title>
+      </ion-toolbar>
+    </ion-header>
     <ion-router-outlet></ion-router-outlet>
+  </ion-page>
   </ion-app>
 </template>
 
 <script setup lang="ts">
-import {IonApp, IonRouterOutlet} from '@ionic/vue'
+import {IonApp, IonRouterOutlet, IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar} from '@ionic/vue';
 </script>
 
 <style scoped>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,23 +1,19 @@
 <template>
 <ion-page>
-  <ion-menu content-id="main-content">
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Menu Content</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content class="ion-padding">This is the menu content.</ion-content>
-  </ion-menu>
-  <ion-content class="ion-padding">
+  <ion-header>
+    <ion-toolbar>
+      <ion-title>Home</ion-title>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content>
     <h1>HOME</h1>
   </ion-content>
 </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonMenu, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+  import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
